70405dfc6813456caa21dec5ef16db8711f418f1
Samuel.Maissen <samuel.maissen@bluewin.ch>
2023-11-21 21:55:04 +0100

    .

--- contracts/EtherTickets.sol
+++ contracts/EtherTickets.sol
@@ -1,16 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
-
+ 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+ 
+contract EtherTickets is ERC721, ERC721Burnable, ERC721Enumerable, Ownable {
+    
+    uint256 private constant maxSupply = 1000;
 
-contract EtherTickets is ERC721, Ownable {
+    
     constructor(address initialOwner)
         ERC721("EtherTickets", "ETT")
         Ownable(initialOwner)
     {}
+ 
+     function mint(uint256 numberOfTokens) public onlyOwner {
+        require(totalSupply() < maxSupply);
+        for (uint256 i = 0; i < numberOfTokens; i++) {
+            _safeMint(msg.sender, totalSupply() + 1);
+        }
+    }
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
+     function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+       function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTExchange is Ownable {
-    uint256 private _listingIds;
+    uint256 public _listingIds;
 
     struct Listing {
         uint256 id;
@@ -18,10 +18,40 @@ contract NFTExchange is Ownable {
 
     mapping(uint256 => Listing) public listings;
 
+    constructor()
+        Ownable(address(this))
+    {}
+
     event ListingCreated(uint256 indexed id, address indexed nftContract, uint256 indexed tokenId, address seller, uint256 price);
     event ListingSold(uint256 indexed id, address indexed buyer);
 
-    constructor(address initialOwner)
-        Ownable(initialOwner)
-    {}
+    function createListing(address nftContract, uint256 tokenId, uint256 price) public {
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        _listingIds += 1;
+
+        listings[_listingIds] = Listing({
+            id: _listingIds,
+            nftContract: nftContract,
+            tokenId: tokenId,
+            seller: payable(msg.sender),
+            price: price,
+            isSold: false
+        });
+
+        emit ListingCreated(_listingIds, nftContract, tokenId, msg.sender, price);
+    }
+
+    function buyNFT(uint256 listingId) public payable {
+        Listing storage listing = listings[listingId];
+        require(!listing.isSold, "NFT is already sold");
+        require(msg.value >= listing.price, "Insufficient funds");
+
+        listing.seller.transfer(listing.price);
+        IERC721(listing.nftContract).transferFrom(address(this), msg.sender, listing.tokenId);
+
+        listing.isSold = true;
+
+        emit ListingSold(listingId, msg.sender);
+    }
 }
